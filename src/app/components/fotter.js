b0ddbe352cc619ef23aa248cd69aa0b75ad53e55
Elias <eliasvitor.dev@gmail.com>
2024-07-28 16:10:54 -0300

    ajustanto nova cor do footter

--- src/app/components/fotter.js
+++ src/app/components/fotter.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Fotter() {
 
     return (
-        <section class="bg-rose-250 dark:bg-gray-900">
-        <div class="container px-6 py-6 mx-auto text-center">
+        <section class="bg-rose-450 pt-8 dark:bg-gray-900 z-0">
+        <div class="container px-6 py-6  mx-auto text-center">
 
         <h3 class="font-sf-new-republic text-xl font-medium text-white lg:text-3xl dark:text-white">Ainda tem duvidas?</h3>
 
@@ -20,18 +20,18 @@ export default function Fotter() {
                         <div class="mx-4">
                             <p class="text-sm text-center text-white dark:text-gray-200">Envie a sua mensagem <br/> e responderemos em até 2 dias úteis</p>
                         </div>
-                        <p class="inline-block mt-4 text-white underline hover:text-blue-400">contato@ongtiniebirdcats.com.br</p>
+                        <p class="inline-block mt-4 text-white underline hover:text-blue-400 cursor-pointer">contato@ongtiniebirdcats.com.br</p>
                     </div>
            
 
          
-                    <div class="flex flex-col p-2 py-4 bg-blue-700 rounded-md justify-center items-center dark:bg-gray-900">
+                    <div class="flex flex-col p-2 py-12 bg-blue-700 rounded-md justify-center items-center dark:bg-gray-900">
                         <svg name="whatsapp" width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M26.8834 22.2069C26.8109 22.1724 24.1142 20.8436 23.6352 20.6721C23.4352 20.5895 23.2225 20.5424 23.0065 20.5327C22.8214 20.536 22.6404 20.5873 22.4812 20.6816C22.3219 20.7758 22.1899 20.9098 22.098 21.0705C21.8285 21.4712 21.0133 22.4246 20.7611 22.709C20.7278 22.747 20.6828 22.7919 20.6564 22.7919C20.6299 22.7919 20.2154 22.6203 20.0887 22.5616C17.9094 21.456 16.0524 19.8065 14.6976 17.7727C14.6758 17.7403 14.6605 17.7039 14.6527 17.6656C14.6963 17.5963 14.7494 17.5335 14.8105 17.4791C14.9578 17.334 15.1167 17.1417 15.271 16.9563C15.3436 16.8688 15.4173 16.7802 15.4887 16.6973C15.6668 16.4888 15.8145 16.2562 15.9274 16.0064L15.9872 15.8855C16.0788 15.7003 16.1233 15.4955 16.1169 15.289C16.1105 15.0826 16.0533 14.8809 15.9504 14.7018C15.8871 14.574 14.7471 11.8232 14.6262 11.5365C14.3349 10.8398 13.9503 10.5151 13.4161 10.5151C13.3665 10.5151 13.4161 10.5151 13.2076 10.5243C12.4314 10.5797 11.6716 10.7747 10.9646 11.1001C10.3732 11.5756 9.90583 12.1874 9.60262 12.883C9.2994 13.5787 9.16936 14.3375 9.22363 15.0944C9.35345 16.8263 10.0041 18.4785 11.0901 19.8338C11.1039 19.8522 11.1293 19.8902 11.1673 19.9489C12.9942 22.8123 15.6119 25.0849 18.7035 26.4915C20.1315 27.1694 21.6771 27.5647 23.2552 27.6556C23.5026 27.6539 23.7497 27.6393 23.9956 27.6118L24.128 27.5992C24.8244 27.4769 25.485 27.2017 26.0624 26.7936C26.6398 26.3854 27.1195 25.8544 27.4672 25.2387C27.8145 24.4674 27.8896 23.6011 27.6802 22.7815C27.4768 22.5159 27.1997 22.316 26.8834 22.2069Z" fill="white"/>
                             <path d="M18.8278 6.51204e-05C14.028 -0.0124944 9.41908 1.87939 6.01232 5.26068C2.60557 8.64196 0.679145 13.2365 0.655681 18.0364C0.653944 21.2449 1.51454 24.395 3.14742 27.157L0.0258391 36.3686C-0.00297413 36.4528 -0.00779736 36.5434 0.0119083 36.6302C0.0316139 36.7171 0.0750722 36.7967 0.137428 36.8602C0.199784 36.9238 0.278581 36.9688 0.365017 36.9901C0.451452 37.0115 0.542122 37.0084 0.626895 36.9811L10.2277 33.931C12.9885 35.4049 16.0827 36.1421 19.2116 36.0713C22.3404 36.0005 25.3982 35.1241 28.0895 33.5268C30.7808 31.9294 33.0147 29.6651 34.5756 26.9524C36.1365 24.2398 36.9715 21.1705 37 18.041C36.9781 13.2402 35.0523 8.64431 31.6453 5.26197C28.2384 1.87963 23.6286 -0.0127953 18.8278 6.51204e-05ZM18.8278 32.3132C16.0135 32.3173 13.2596 31.4962 10.907 29.9515C10.8474 29.9123 10.7797 29.8869 10.709 29.8772C10.6383 29.8674 10.5663 29.8736 10.4983 29.8951L5.68981 31.4242L7.24197 26.8438C7.26659 26.7708 7.27327 26.6929 7.26145 26.6167C7.24962 26.5406 7.21964 26.4684 7.17403 26.4062C5.38918 23.981 4.42871 21.0476 4.43358 18.0364C4.41077 15.1849 5.23548 12.3909 6.80309 10.0088C8.37069 7.62681 10.6105 5.7641 13.2384 4.65705C15.8663 3.54999 18.7639 3.24848 21.5634 3.79077C24.3629 4.33306 26.9383 5.69471 28.9627 7.70296C30.9872 9.71121 32.3695 12.2755 32.9342 15.0706C33.499 17.8657 33.2208 20.7656 32.1348 23.4023C31.0489 26.039 29.2043 28.2937 26.8349 29.8804C24.4655 31.4671 21.6783 32.3142 18.8267 32.3143L18.8278 32.3132Z" fill="white"/>
                         </svg>
                         <p class="mt-6 text-xl font-semibold  text-white dark:text-white">Por Whatsapp</p>
-                        <p class="inline-block mt-4 text-white underline hover:text-blue-400">Chame no  (13) 91234-5678</p>
+                        <p class="inline-block mt-4 text-white underline hover:text-blue-400 cursor-pointer">Chame no  (13) 91234-5678</p>
                     </div>
 
                 <div class="flex flex-col px-3 justify-center items-center dark:bg-gray-900">
@@ -46,7 +46,7 @@ export default function Fotter() {
                         <div class="mx-4">
                             <p class="text-sm text-center text-white dark:text-gray-200">Utilize nossas redes para entrar em <br /> contato conosco! </p>
                         </div>
-                        <div class="flex gap-2 my-2 mt-4text-white underline">
+                        <div class="flex gap-2 my-2 mt-4text-white underline cursor-pointer">
                             <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <circle cx="18" cy="18" r="18" fill="white"/>
                                 <path d="M24.5 14.9019C26.5 16.0566 26.5 18.9434 24.5 20.0981L16.25 24.8612C14.25 26.0159 11.75 24.5725 11.75 22.2631L11.75 12.7369C11.75 10.4275 14.25 8.98408 16.25 10.1388L24.5 14.9019Z" fill="#E6807D"/>
@@ -74,12 +74,12 @@ export default function Fotter() {
            
             <div class="w-1/2 relative flex items-center mt-2">
                 <button class="absolute right-0 focus:outline-none rtl:left-0 rtl:right-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mx-4 text-gray-400 transition-colors duration-300 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400">
-                        <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                        <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
-                    </svg>
+                <svg class="w-6 h-6 mr-2 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/>
+                </svg>
+
                 </button>
-                <input type="email" placeholder="Digite seu e-mail" class="block w-full py-2 text-white placeholder-white bg-rose-250 border border-gray-200 rounded-md pl-5 pr-11 rtl:pr-5 rtl:pl-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                <input type="email" placeholder="Digite seu e-mail" class="block w-full py-2 text-white placeholder-white bg-transparent border border-gray-200 rounded-md pl-5 pr-11 rtl:pr-5 rtl:pl-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
             </div>
         </div>
     </div>
